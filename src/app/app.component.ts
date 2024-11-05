62d03442b31b51effefc8e2bedf66402f6b759e8
Guillaume1979 <guibetguillaume@gmail.com>
2024-11-05 15:15:55 +0100

    Feature: Migrate to SCSS and integrate Angular Material, and add dark-mode handling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, effect, signal} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {MatButton} from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatButton],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'material-theming';
+  darkMode = signal(false)
+
+  setDarkMode = effect(() => {
+    document.documentElement.classList.toggle('dark', this.darkMode());
+  })
 }
